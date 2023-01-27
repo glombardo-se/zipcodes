e7e196f3d95fd6325dd6c7c18d7b5ae7fe239b51
g.lombardo <g.lombardo@pm.me>
2023-01-27 10:43:12 +0100

    Added countrycodes.py Added countrynames.py Added countrycode and countrynames to the data package.

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from countrycodes import countrycodes
+from countrynames import countrynames
 
 zipcodes = Path(__file__).parent / 'zipcodes.db'
 zipcodes_IT = Path(__file__).parent / 'zipcodes_IT.db'
